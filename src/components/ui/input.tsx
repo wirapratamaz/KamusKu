a0a8a46ca259fd7dc4f01e7e9331c136682718c2
wirapratamaz <89260515+wirapratamaz@users.noreply.github.com>
2024-10-18 01:29:26 +0800

    fix ESLint rule for interface

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,8 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -20,6 +19,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     )
   }
 )
+
 Input.displayName = "Input"
 
 export { Input }
